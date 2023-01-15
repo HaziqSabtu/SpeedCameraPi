25094bbb7a6c58a247dd9a0e90937d058bd804de
haziqsabtu <kaki.kapal00@gmail.com>
2023-01-15 12:36:59 +0100

    fix bug with Optical Flow

---
+++ src/Algorithm/optical_flow/SparseOpticalFlow.cpp
@@ -0,0 +1,165 @@
+#include <Algorithm/optical_flow/SparseOpticalFlow.hpp>
+
+SparseOF::SparseOF() : criteria(criteriaType, maxCount, epsilon), rng() {
+    for (int i = 0; i < 100; i++) {
+        int r = rng.uniform(0, 256);
+        int g = rng.uniform(0, 256);
+        int b = rng.uniform(0, 256);
+        colors.push_back(cv::Scalar(r, g, b));
+    }
+}
+
+SparseOF::~SparseOF() {}
+
+void SparseOF::setInitialPoints() {
+    cv::goodFeaturesToTrack(prevGray, initialPoints, maxCorners, qualityLevel,
+                            minDistance, cv::Mat(), blockSize,
+                            useHarrisDetector, k);
+
+    // todo: fix cornerSubPix to increase accuracy
+    cv::cornerSubPix(prevGray, initialPoints, cv::Size(5, 5), cv::Size(-1, -1),
+                     criteria);
+}
+
+void SparseOF::setRoiPoints(const cv::Rect &roi) {
+    this->roi = roi;
+    for (int i = 0; i < initialPoints.size(); i++) {
+        if (roi.contains(initialPoints[i])) {
+            roiPoints.push_back(initialPoints[i]);
+        }
+    }
+    pushCollection(roiPoints);
+}
+
+void SparseOF::Init(const cv::Mat &frame, cv::Rect &roi) {
+    cv::cvtColor(frame, prevGray, cv::COLOR_BGR2GRAY);
+    setInitialPoints();
+    setRoiPoints(roi);
+    mask = cv::Mat::zeros(frame.size(), frame.type());
+}
+
+void SparseOF::run(const cv::Mat &frame) {
+    clearVector();
+
+    cv::Mat frameGray;
+    cv::cvtColor(frame, frameGray, cv::COLOR_BGR2GRAY);
+
+    cv::calcOpticalFlowPyrLK(prevGray, frameGray, roiPoints, trackingPoints,
+                             status, err);
+    pushCollection(trackingPoints, status, err);
+    drawPoints(frame);
+
+    updatePrevVariables(frameGray, trackingPoints);
+}
+
+void SparseOF::drawPoints(const cv::Mat &frame) {
+    for (int i = 0; i < trackingPoints.size(); i++) {
+        cv::line(mask, trackingPoints[i], roiPoints[i], colors[i], 2);
+        cv::circle(frame, trackingPoints[i], 5, colors[i], -1);
+    }
+    cv::add(frame, mask, output);
+}
+
+void SparseOF::updatePrevVariables(const cv::Mat &g,
+                                   const std::vector<cv::Point2f> &p) {
+    prevGray.release();
+    g.copyTo(prevGray);
+
+    roiPoints.assign(p.begin(), p.end());
+}
+
+void SparseOF::pushCollection(std::vector<cv::Point2f> &points) {
+    std::vector<PointData> pointData;
+    for (int i = 0; i < points.size(); i++) {
+        pointData.push_back(PointData{points[i], 1});
+    }
+    collection.push_back(pointData);
+}
+
+void SparseOF::pushCollection(std::vector<cv::Point2f> &points,
+                              std::vector<uchar> &status,
+                              std::vector<float> &err) {
+    std::vector<PointData> pointData;
+    for (int i = 0; i < points.size(); i++) {
+        pointData.push_back(PointData{points[i], status[i], err[i]});
+    }
+    collection.push_back(pointData);
+}
+
+void SparseOF::clearVector() {
+    status.clear();
+    err.clear();
+    trackingPoints.clear();
+    // good_new.clear();
+}
+
+void SparseOF::frameSkip(std::vector<std::vector<PointData>> &pointData,
+                         int skip) {
+    std::vector<std::vector<PointData>> temp;
+    for (int i = 0; i < pointData.size(); i++) {
+        if (i % skip == 0) {
+            temp.push_back(pointData[i]);
+        }
+    }
+    pointData = temp;
+}
+
+bool SparseOF::isROIEmpty() {
+    if (roiPoints.size() == 0) {
+        std::cout << "No Points Available for Tracking" << std::endl;
+        return true;
+    }
+
+    if (roiPoints.size() < 3) {
+        std::cout << "Not Enough Points Available for Tracking" << std::endl;
+        return true;
+    }
+    for (auto &point : roiPoints) {
+        std::cout << "ROI: " << point << std::endl;
+    }
+    pushCollection(roiPoints);
+    return false;
+}
+
+std::vector<std::vector<PointData>> SparseOF::evaluateCollection() {
+    std::vector<std::vector<PointData>> pass =
+        std::vector<std::vector<PointData>>(collection.size());
+    std::vector<int> passPos;
+    const int MAXERROR = 20;
+    for (int i = 0; i < collection[0].size(); i++) {
+        for (int j = 0; j < collection.size(); j++) {
+            if (collection[j][i].status == 1) {
+                if (collection[j][i].err > MAXERROR) {
+                    break;
+                }
+            }
+            if (j == collection.size() - 1) {
+                passPos.push_back(i);
+            }
+        }
+    }
+
+    for (auto &pos : passPos) {
+        std::cout << "Pass: " << pos << std::endl;
+        for (int i = 0; i < collection.size(); i++) {
+            pass[i].push_back(collection[i][pos]);
+        }
+    }
+
+    std::cout << "Pass Size: " << pass.size() << std::endl;
+    std::cout << "Pass[0] Size: " << pass[0].size() << std::endl;
+
+    std::cout << "Collection Size: " << collection.size() << std::endl;
+    std::cout << "Collection[0] Size: " << collection[0].size() << std::endl;
+    return pass;
+}
+
+std::vector<std::vector<PointData>> SparseOF::getCollection() {
+    return collection;
+};
+
+cv::Rect SparseOF::getRoi() { return roi; }
+
+cv::Mat SparseOF::getOutput() { return output; }
+
+cv::Mat SparseOF::getMask() { return mask; }
--- src/UI/OpticalFlowPanel/ImagePanel.cpp
+++ src/UI/OpticalFlowPanel/ImagePanel.cpp
@@ -6,21 +6,15 @@ OpticalFlowPanelImage::OpticalFlowPanelImage(wxWindow *parent, wxWindowID id,
     this->imgData = imgData;
     cv::Mat firstImg = imgData[count].image;
 
+    opticalFlow = new SparseOF();
+
     tracker = new OBJECTTRACKER("CSRT");
 
     img_bitmap = new BBOpticalFlow(this, wxID_ANY);
     img_bitmap->SetImage(firstImg);
 }
 
-void OpticalFlowPanelImage::OnKeyPress(wxKeyEvent &e) {
-    // if (e.GetKeyCode() == 'n' || e.GetKeyCode() == WXK_RIGHT) {
-    //     OnButtonIncrement();
-    // } else if (e.GetKeyCode() == 'p' || e.GetKeyCode() == WXK_LEFT) {
-    //     OnButtonDecrement();
-    // } else if (e.GetKeyCode() == 's' || e.GetKeyCode() == WXK_RETURN) {
-    //     // todo set selected IMG
-    // }
-}
+void OpticalFlowPanelImage::OnKeyPress(wxKeyEvent &e) {}
 
 void OpticalFlowPanelImage::OnSize(wxSizeEvent &e) {
     wxSize this_size = e.GetSize();
@@ -38,10 +32,17 @@ void OpticalFlowPanelImage::SetTrueRect(cv::Rect rect) {
 
 void OpticalFlowPanelImage::OnButtonIncrement() {
     count = (count >= imgData.size() - 1) ? imgData.size() - 1 : count + 1;
+    if (isOpticalFlow) {
+        cv::Mat f = imgData[count].image;
+        opticalFlow->run(f);
+        img_bitmap->SetImage(opticalFlow->getOutput());
+        return;
+    }
     if (isObjectTracked) {
         cv::Rect r = tracker->UpdateTracker(imgData[count].image);
         img_bitmap->SetTrueRect(r);
     }
+    wxMessageBox("Normal");
     img_bitmap->SetImage(imgData[count].image);
 }
 
@@ -59,6 +60,26 @@ void OpticalFlowPanelImage::StartTracking() {
         tracker->InitTracker(imgData[count].image, img_bitmap->GetTrueRect());
         isObjectTracked = true;
     }
+}
+
+void OpticalFlowPanelImage::StartOpticalFlow() {
+
+    cv::Mat f = imgData[count].image;
+    cv::Rect r = img_bitmap->GetTrueRect();
+    opticalFlow->Init(f, r);
+
+    while (opticalFlow->getCollection().size() < MAX_COUNT) {
+        count = (count >= imgData.size() - 1) ? imgData.size() - 1 : count + 1;
+        f = imgData[count].image;
+        opticalFlow->run(f);
+        img_bitmap->SetImage(opticalFlow->getOutput());
+    }
+    // if (!isOpticalFlow) {
+    //     cv::Mat f = imgData[count].image.clone();
+    //     cv::Rect r = img_bitmap->GetTrueRect();
+    //     opticalFlow->Init(f, r);
+    //     isOpticalFlow = true;
+    // }
 }
 
 // clang-format off
