417938e5deb62431336584afed4b830cc3ed6654
HaziqSabtu <kaki.kapal00@gmail.com>
2023-10-15 13:32:27 +0200

    cxcmpl

--- scripts/crosscompile/Target_cvinstall.py
+++ scripts/crosscompile/Target_cvinstall.py
@@ -14,20 +14,20 @@ if cUtils.isRunningWithSudo():
 
 def update_sources(credentials):
     commands = [
-        'sudo apt-get install -y build-essential cmake git unzip pkg-config'
-        'sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev'
-        'sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev'
-        'sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev'
-        'sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3'
-        'sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl'
-        'sudo apt-get install -y libxvidcore-dev libx264-dev'
-        'sudo apt-get install -y python3-dev python3-numpy python3-pip'
-        'sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev'
-        'sudo apt-get install -y libv4l-dev v4l-utils'
-        'sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev'
-        'sudo apt-get install -y liblapack-dev gfortran libhdf5-dev'
-        'sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev'
-        'sudo apt-get install -y protobuf-compiler'
+        'sudo apt install -y build-essential cmake git unzip pkg-config'
+        'sudo apt install -y libjpeg-dev libtiff-dev libpng-dev'
+        'sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev'
+        'sudo apt install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev'
+        'sudo apt install -y libgstreamer1.0-dev gstreamer1.0-gtk3'
+        'sudo apt install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl'
+        'sudo apt install -y libxvidcore-dev libx264-dev'
+        'sudo apt install -y python3-dev python3-numpy python3-pip'
+        'sudo apt install -y libtbb2 libtbb-dev libdc1394-22-dev'
+        'sudo apt install -y libv4l-dev v4l-utils'
+        'sudo apt install -y libopenblas-dev libatlas-base-dev libblas-dev'
+        'sudo apt install -y liblapack-dev gfortran libhdf5-dev'
+        'sudo apt install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev'
+        'sudo apt install -y protobuf-compiler'
     ]
 
     for command in commands:
